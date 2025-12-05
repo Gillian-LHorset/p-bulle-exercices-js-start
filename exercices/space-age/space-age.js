a7f5dba41018b2a852e2278fba8c45d885cfe5eb
Gillian L' Horset <pq60soi@eduvaud.ch>
2025-12-05 15:55:30 +0100

    feat(space age): Réalisation de l'exercice space-age.js
    [50][DONE]

--- exercices/space-age/space-age.js
+++ exercices/space-age/space-age.js
@@ -3,6 +3,12 @@
 // convenience to get you started writing code faster.
 //
 
-export const age = () => {
-  throw new Error('Remove this line and implement the function');
+export const age = (planet, age) => {
+  const planetArray = ["earth", "mercury", "venus", "mars", "jupiter", "saturn", "uranus", "neptune"]
+  const orbitalPeriodArray = [1.0 , 0.2408467, 0.61519726, 1.8808158, 11.862615, 29.447498, 84.016846, 164.79132]
+  if (planet == "Sun" || planet == "pluton") {
+    throw new Error ('not a planet')
+  } else {
+    return (Number)((age / (orbitalPeriodArray[planetArray.indexOf(planet)] * 60 * 60 * 24 * 365.25)).toFixed(2))
+  }
 };
