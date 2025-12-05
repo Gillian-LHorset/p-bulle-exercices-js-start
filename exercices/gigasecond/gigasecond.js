63ada918e8cf9f6348a0fb6e4bbe541f0dc24207
Gillian L' Horset <pq60soi@eduvaud.ch>
2025-12-05 14:34:33 +0100

    fix(giga second): amélioration de la lisibilité du code

--- exercices/gigasecond/gigasecond.js
+++ exercices/gigasecond/gigasecond.js
@@ -2,7 +2,8 @@
 // This is only a SKELETON file for the 'Gigasecond' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
-const oneGigaSecond = 1000000000000;
+const oneGigaSecond = 1 * Math.pow(10, 12);
 export const gigasecond = (date) => {
+  // puissance 12 et non puissance 9 parce que Date prend en compte les millisecond
   return new Date(date.getTime() + oneGigaSecond)
 };
